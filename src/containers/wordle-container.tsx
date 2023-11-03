12c313fbcfa18725a8ab40bd59d44769f2726ba2
alimoqimi97 <41248132+alimoqimi97@users.noreply.github.com>
2023-11-03 15:26:05 +0330

    Feat(implement-wordle): Implement WordleContainer
    - add required packages
    - Implement Key component
    - Implement Keyboard component
    - Implement required files

---
+++ src/containers/wordle-container.tsx
@@ -0,0 +1,110 @@
+import { FC } from "react";
+import { Keyboard } from "@components/WordleContainer/KeyBoard";
+import { solution, unicodeLength } from "@lib/words";
+import styles from "./styles.module.scss";
+
+const useKeyBoard = () => {
+  const onChar = (value: string) => {
+    if (
+      unicodeLength(`${currentGuess}${value}`) <= solution.length &&
+      guesses.length < MAX_CHALLENGES &&
+      !isGameWon
+    ) {
+      setCurrentGuess(`${currentGuess}${value}`);
+    }
+  };
+
+  const onDelete = () => {
+    setCurrentGuess(
+      new GraphemeSplitter().splitGraphemes(currentGuess).slice(0, -1).join("")
+    );
+  };
+
+  const onEnter = () => {
+    if (isGameWon || isGameLost) {
+      return;
+    }
+
+    if (!(unicodeLength(currentGuess) === solution.length)) {
+      setCurrentRowClass("jiggle");
+      return showErrorAlert(NOT_ENOUGH_LETTERS_MESSAGE, {
+        onClose: clearCurrentRowClass,
+      });
+    }
+
+    if (!isWordInWordList(currentGuess)) {
+      setCurrentRowClass("jiggle");
+      return showErrorAlert(WORD_NOT_FOUND_MESSAGE, {
+        onClose: clearCurrentRowClass,
+      });
+    }
+
+    // enforce hard mode - all guesses must contain all previously revealed letters
+    if (isHardMode) {
+      const firstMissingReveal = findFirstUnusedReveal(currentGuess, guesses);
+      if (firstMissingReveal) {
+        setCurrentRowClass("jiggle");
+        return showErrorAlert(firstMissingReveal, {
+          onClose: clearCurrentRowClass,
+        });
+      }
+    }
+
+    setIsRevealing(true);
+    // turn this back off after all
+    // chars have been revealed
+    setTimeout(() => {
+      setIsRevealing(false);
+    }, REVEAL_TIME_MS * solution.length);
+
+    const winningWord = isWinningWord(currentGuess);
+
+    if (
+      unicodeLength(currentGuess) === solution.length &&
+      guesses.length < MAX_CHALLENGES &&
+      !isGameWon
+    ) {
+      setGuesses([...guesses, currentGuess]);
+      setCurrentGuess("");
+
+      if (winningWord) {
+        if (isLatestGame) {
+          setStats(addStatsForCompletedGame(stats, guesses.length));
+        }
+        return setIsGameWon(true);
+      }
+
+      if (guesses.length === MAX_CHALLENGES - 1) {
+        if (isLatestGame) {
+          setStats(addStatsForCompletedGame(stats, guesses.length + 1));
+        }
+        setIsGameLost(true);
+        showErrorAlert(CORRECT_WORD_MESSAGE(solution), {
+          persist: true,
+          delayMs: REVEAL_TIME_MS * solution.length + 1,
+        });
+      }
+    }
+  };
+
+  return { onChar, onDelete, onEnter };
+};
+
+const WordleContainer: FC = () => {
+  const { onChar, onDelete, onEnter } = useKeyBoard();
+
+  return (
+    <section className={styles["wordle-container"]}>
+      <Keyboard
+        onChar={onChar}
+        onDelete={onDelete}
+        onEnter={onEnter}
+        solution={solution}
+        guesses={guesses}
+        isRevealing={isRevealing}
+      />
+    </section>
+  );
+};
+
+export default WordleContainer;
